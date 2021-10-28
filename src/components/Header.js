e60d9d007b48113b22e953dd74b62e953cabaf6c
Mark Pieciak <mpieciak18@msn.com>
2021-10-28 01:53:25 -0400

    add non-functional preview & download buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,18 @@ class Header extends React.Component {
     render() {
         return(
             <div id='header'>
-                <img src={process.env.PUBLIC_URL + '/images/icon.png'} alt='page icon' width='35' height='35' />
-                <h1>Résumé Maker</h1>
+                <div id='header-left'>
+                    <img id='icon' src={process.env.PUBLIC_URL + '/images/icon.png'} alt='page icon' />
+                    <h1 id='title'>Résumé Maker</h1>
+                </div>
+                <div id='header-right'>
+                    <div id='preview-area'>
+                        <p>Preview Off</p>
+                        <input type="checkbox" id="preview-button" class="toggle-input" />
+                        <label class="preview-button toggle-label" for="preview-button">Toggle</label>
+                    </div>
+                    <button id='download-button' type='button'>Download PDF</button>
+                </div>
             </div>
         )
     }
